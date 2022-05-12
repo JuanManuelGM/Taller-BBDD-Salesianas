6f7e1fa614b49941e789fc257c0e5ed96b5ed2ea
Solid-Lizard <95529607+Solid-Lizard@users.noreply.github.com>
2022-05-12 18:11:10 +0200

    Añadido PL/SQL sobre estudio de contagios y errores corregidos en consultas complejas.sql

--- BBDD/Consultas/complejas.sql
+++ BBDD/Consultas/complejas.sql
@@ -4,34 +4,19 @@
 -- RESPONSABLE : SALESIANAS Mª AUXILIADORA
 ---------------------------------------------------------
 
---Muestra los contagios totales a lo largo de un año, junto a la población, nombre y fecha de los registros de cada ciudad de una provincia específica--
+--Muestra los contagios totales a lo largo de un año, junto a la población, nombre y fecha de los registros de cada ciudad de una comunidad autónoma específica--
 SELECT CIUDAD.NOMBRE CIUDAD, CIUDAD.POBLACION POBLACION, CONTAGIOS.CONTAGIOS_TOTALES, CONTAGIOS.FECHA 
 	FROM CIUDAD 
 	LEFT JOIN CONTAGIOS ON CIUDAD.ID_CIUDAD = CONTAGIOS.ID_CIUDAD 
-	WHERE YEAR(FECHA)=2020 AND PROVINCIA LIKE 'AndalucÃ­a';
+	WHERE YEAR(FECHA)=2020 AND COMUNIDAD_AUTONÓNOMA LIKE 'Andalucía';
 	
---Muestra el recuento total de vacunados (en número y en porcentaje), con respecto a la población desde el principio de la pandemia hasta la actualidad de todas las ciudades de una provincia específica.
+--Muestra el recuento total de vacunados (en número y en porcentaje), con respecto a la población desde el principio de la pandemia hasta la actualidad de todas las ciudades de una comunidad autónoma específica.
 SELECT CIUDAD.NOMBRE AS NOMBRE, CIUDAD.POBLACION AS POBLACION, SUM(VACUNACIONES.VACUNACIONES_TOTALES) AS VACUNACIONES, (SUM(VACUNACIONES.VACUNACIONES_TOTALES)*100)/CIUDAD.POBLACION AS PORCENTAJE  
 	FROM VACUNACIONES RIGHT JOIN CIUDAD ON VACUNACIONES.ID_CIUDAD=CIUDAD.ID_CIUDAD 
 	WHERE CIUDAD.PROVINCIA LIKE 'AndalucÃ­a' 
 	GROUP BY CIUDAD.NOMBRE, CIUDAD.POBLACION;
 	
--- Muestra, de cada centro de vacunacion, la direccion, el nombre, el aforo, la provincia a la que pertenecen y los nombres de las vacunas que reciben
+-- Muestra, de cada centro de vacunacion, la direccion, el nombre, el aforo, la comunidad autónoma a la que pertenecen y los nombres de las vacunas que reciben
 SELECT CIUDAD.PROVINCIA AS PROVINCIA, CENTRO_VACUNACION.NOMBRE AS CENTRO, CENTRO_VACUNACION.DIRECCION AS DIRECCION, CENTRO_VACUNACION.AFORO AS AFORO, DISTRIBUIDOR_VACUNA.NOMBRE AS VACUNA 
 	FROM CIUDAD LEFT JOIN CENTRO_VACUNACION ON CIUDAD.ID_CIUDAD=CENTRO_VACUNACION.ID_CIUDAD 
 	LEFT JOIN DISTRIBUIDOR_VACUNA ON CENTRO_VACUNACION.ID_CENTRO=DISTRIBUIDOR_VACUNA.ID_CENTRO ORDER BY PROVINCIA;
-	
-	
-	
-	------------------------------------------------------------------------------------------------
-	
-	-- LAS CONSULTAS DE ABAJO QUIERO HACERLAS PERO TODAVIA NO SE COMO --
-	
-
---Muestra, de las ciudades de una provincia, la poblacion, los contagios totales y las capas ocupadas en un mes--
-	
---Muestra la evolución de vacunados y contagiados en una ciudad a lo largo de un año de una ciudad--
-
---Muestra una comparativa de datos de contagiados y vacunados de una ciudad con un mes respecto al anterior (incluyendo cuanto han subido o bajado los vacunados y contagiados)--ç
-
---Control de contagiados con respecto al mes anterior en una ciudad--
--- BBDD/Creación/PL-SQL.sql
+++ BBDD/Creación/PL-SQL.sql
@@ -19,3 +19,57 @@ END homeArrest;
 
 --Se realiza la prueba para confinar las ciudades con un número de contagios superior a 5000, en este caso, Sevilla--
 EXECUTE HOMEARREST(5000);
+
+-------------------------------------------------------------------------------------------------------------------------
+
+--Con la intención de facilitar el control de la evolución de la pandemia, se crea un procedimiento
+--que, dadas dos fechas y un ID de una ciudad, muestra los datos de nuevos contagios, junto con el procentaje de poblacion
+--infectada del mes
+
+create or replace PROCEDURE estudio (inicio DATE, fin DATE, idCiudad NUMBER)
+IS
+    CURSOR cityInfections IS
+        SELECT CIUDAD.NOMBRE, CONTAGIOS.contagios_totales, CONTAGIOS.fecha
+        FROM CIUDAD LEFT JOIN CONTAGIOS ON CIUDAD.ID_CIUDAD = CONTAGIOS.ID_CIUDAD
+        WHERE CIUDAD.ID_CIUDAD=idCiudad AND CONTAGIOS.FECHA BETWEEN inicio AND fin;
+
+    nombreCiudad VARCHAR(40);    
+    contiene BOOLEAN := FALSE;  
+
+    habitantes NUMBER(38);
+    porcentaje DECIMAL(38,2);
+
+BEGIN    
+
+   SELECT NOMBRE INTO nombreCiudad FROM CIUDAD
+   WHERE ID_CIUDAD=idCiudad;  
+
+dbms_output.put_line('------Estudio de la ciudad de: ' || nombreCiudad || '------');    
+
+FOR mes IN cityInfections
+LOOP                        
+    dbms_output.put_line(habitantes);  
+    SELECT POBLACION INTO habitantes FROM CIUDAD WHERE ID_CIUDAD=idCiudad;    
+   
+    porcentaje := (mes.contagios_totales*100)/habitantes;
+   
+    dbms_output.put_line( '--------------------------------------------------');
+    dbms_output.put_line( 'FECHA: ' || mes.fecha);    
+    dbms_output.put_line( 'Contagios totales: ' || mes.contagios_totales);  
+     
+    IF porcentaje<1
+    THEN
+        dbms_output.put_line('Porcentaje de población nueva infectada: 0' || porcentaje || '%');
+    ELSE
+        dbms_output.put_line('Porcentaje de población nueva infectada: ' || porcentaje || '%');
+    END IF;
+     
+    dbms_output.put_line( '--------------------------------------------------');
+
+END LOOP;
+
+END estudio;
+
+
+--Comprobamos los contagiados de la ciudad de ID 3
+execute estudio('25-08-2020', '25-10-2020', 3);
